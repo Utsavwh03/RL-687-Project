4d5f1c4de8daab2247b013b15550eb49d7a092bf
utsavdhanuka03 <utsavdhanuka03@kgpian.iitkgp.ac.in>
2025-12-04 13:03:44 -0500

    Added Models

---
+++ models.py
@@ -0,0 +1,30 @@
+# Implmenting the policy and value networks
+
+import torch 
+import torch.nn as nn 
+import torch.optim as optim 
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim,hidden_dim=64):
+        super(PolicyNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        self.relu = nn.ReLU()
+
+        
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x =self.fc2(x)
+        return x
+    
+class ValueNetwork(nn.Module):
+    def __init__(self, state_dim, hidden_dim=64):
+        super(ValueNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
